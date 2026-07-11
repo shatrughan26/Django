d608aa4180e6f0ffd276f56950d2fe0fcb67b91f
Deepak kumar sharma <deepak4996sharma@gmail.com>
2026-07-11 12:12:59 +0530

    adding console based email service for forgot password

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -130,3 +130,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
